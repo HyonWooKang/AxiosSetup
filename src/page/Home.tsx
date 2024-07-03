88d45e0a2119692f36ac05a6062c3e16d6cda752
HyonWooKang <87benzenes@gmail.com>
2024-07-03 18:53:32 +0900

    Home 화면 작성 중

---
+++ src/page/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <main className="container">
+          <div className="header">
+            <div className="top"></div>
+          </div>
+          <div className="body"></div>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Home;
